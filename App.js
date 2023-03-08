3ac2e6e7c4fbb0f6e50bd00247d445aaeb8895ee
Krissy Macdonald <krissymacdonald5@gmail.com>
2023-03-08 03:14:11 -0500

    project initialized

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.welcome} > coming soon! </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome: {
+    color: '#e8400c',
+    fontWeight: 'bold',
+  }
 });
